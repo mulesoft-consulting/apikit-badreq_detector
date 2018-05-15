7cf9e0d273fbad7199b1f278814a3508adc62d68
Brady Wied <brady@bswtechconsulting.com>
2018-05-15 09:58:26 -0600

    Separate setup code

--- src/test/java/com/mulesoft/ps/util/ApikitBadRequestDetectorTest.groovy
+++ src/test/java/com/mulesoft/ps/util/ApikitBadRequestDetectorTest.groovy
@@ -1,10 +1,14 @@
 package com.mulesoft.ps.util
 
 import groovy.json.JsonOutput
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Test
 import org.mule.DefaultMuleEvent
 import org.mule.DefaultMuleMessage
 import org.mule.MessageExchangePattern
+import org.mule.api.MuleContext
+import org.mule.api.MuleEvent
 import org.mule.api.transport.PropertyScope
 import org.mule.config.spring.SpringXmlConfigurationBuilder
 import org.mule.construct.Flow
@@ -14,14 +18,26 @@ import org.mule.module.apikit.exception.BadRequestException
 import static groovy.test.GroovyAssert.shouldFail
 
 class ApikitBadRequestDetectorTest {
-    @Test
-    void parse() {
-        // arrange
+    private static Flow flow
+    private static MuleContext muleContext
+
+    @BeforeClass
+    static void setup() {
         def factory = new DefaultMuleContextFactory()
         def configBuilder = new SpringXmlConfigurationBuilder('stuff.xml')
-        def muleContext = factory.createMuleContext(configBuilder)
+        muleContext = factory.createMuleContext(configBuilder)
         muleContext.start()
-        def message = new DefaultMuleMessage(JsonOutput.toJson([howdy: 123]), muleContext)
+        flow = muleContext.registry.get('api-main') as Flow
+    }
+
+    @AfterClass
+    static void cleanup() {
+        muleContext.stop()
+        muleContext.dispose()
+    }
+
+    static MuleEvent getEvent(Map payload) {
+        def message = new DefaultMuleMessage(JsonOutput.toJson(payload), muleContext)
         def inboundProp = { String prop, String value ->
             message.setProperty(prop,
                                 value,
@@ -38,12 +54,18 @@ class ApikitBadRequestDetectorTest {
         httpProp('method', 'POST')
         inboundProp('host', 'localhost')
         inboundProp('Content-Type', 'application/json')
-        def flow = muleContext.registry.get('api-main') as Flow
-        def event = new DefaultMuleEvent(message,
-                                         MessageExchangePattern.REQUEST_RESPONSE,
-                                         flow)
+        new DefaultMuleEvent(message,
+                             MessageExchangePattern.REQUEST_RESPONSE,
+                             flow)
+    }
+
+
+    @Test
+    void parse() {
+        // arrange
+        def inputEvent = getEvent([howdy: 123])
         def messageException = shouldFail {
-            flow.process(event)
+            flow.process(inputEvent)
         }
         def badRequestException = messageException.cause as BadRequestException
         def connector = new ApikitBadRequestDetector()
