12bd2b16ad6b12ed01c104859252f2c1e23bcbfa
Brady Wied <brady@bswtechconsulting.com>
2018-05-15 12:03:19 -0600

    Use map/reduce style

--- src/main/java/com/mulesoft/ps/util/ExceptionTransformer.groovy
+++ src/main/java/com/mulesoft/ps/util/ExceptionTransformer.groovy
@@ -5,27 +5,23 @@ import org.mule.module.apikit.exception.BadRequestException
 import java.util.regex.Pattern
 
 class ExceptionTransformer {
-    private static final Pattern requiredPattern = Pattern.compile(/Missing required field "(.*?)"/,
-                                                                   Pattern.DOTALL)
-    private static final Pattern invalidTypePattern = Pattern.compile(/Invalid type (\S+), expected (\S+)/,
-                                                                      Pattern.DOTALL)
+    private static final Pattern requiredPattern = Pattern.compile(/Missing required field "(.*?)"/)
+    private static final Pattern invalidTypePattern = Pattern.compile(/Invalid type (\S+), expected (\S+)/)
 
     static List<FieldError> transform(BadRequestException badRequestException) {
-        def message = badRequestException.message
-        List<FieldError> results = []
-        message.split("\n").each { String errorForField ->
+        badRequestException.message.split("\n")
+                .collect { String errorForField ->
             def matcher = requiredPattern.matcher(errorForField)
-            while (matcher.find()) {
-                results << new FieldError(matcher.group(1),
-                                          'field is required and is missing')
+            if (matcher.find()) {
+                return new FieldError(matcher.group(1),
+                                      'field is required and is missing')
             }
             matcher = invalidTypePattern.matcher(errorForField)
-            while (matcher.find()) {
-                results << new FieldError('(Unknown field name)',
-                                          "Expected type '${matcher.group(2)}' but got '${matcher.group(1)}'")
+            if (matcher.find()) {
+                return new FieldError('(Unknown field name)',
+                                      "Expected type '${matcher.group(2)}' but got '${matcher.group(1)}'")
             }
-        }
-        results.sort { FieldError error ->
+        }.sort { FieldError error ->
             error.fieldName
         }
     }
